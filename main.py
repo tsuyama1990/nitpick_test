1a35e46d9a7e75b1daedb165cc3611827c419ab0
tsuyama1990 <81780308+tsuyama1990@users.noreply.github.com>
2026-05-13 02:59:17 +0000

    feat: cycle 04 local parquet caching implementation

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-
 def main() -> None:
     logger.info("Hello from app!")
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
